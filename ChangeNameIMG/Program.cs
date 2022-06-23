7346a97a05362e8e8fd0002a020650d81c778f8f
Nguyen Quang Thanh <quangthanhn485@gmail.com>
2022-06-24 01:52:25 +0700

    Add project files.

---
+++ ChangeNameIMG/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeNameIMG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = @"E:\work\anh cong trinh 4\";
+            DirectoryInfo d = new DirectoryInfo(folder); //Assuming Test is your Folder
+
+            FileInfo[] Files = d.GetFiles();//GetFiles(".exe"); //Getting Text files
+            string str = "";
+            int count = 0;
+            foreach (FileInfo filee in Files)
+            {
+                str = str + ", " + filee.Name;
+                //System.IO.File.Move(folder+filee.Name, folder+filee.Name+ ".jpg");
+                count++;
+            }
+            Console.WriteLine("count : "+count);
+            Console.WriteLine("xong");
+            Console.Read();
+        }
+    }
+}
